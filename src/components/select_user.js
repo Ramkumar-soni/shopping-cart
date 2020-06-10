f856f28419579bd04ff0a10a14d0b291cdd0566d
Ramkumar-soni <57743054+Ramkumar-soni@users.noreply.github.com>
2020-06-10 13:55:37 +0530

    Revert "Updated filea and added gitignore"
    This reverts commit 7d601fad8e018eb76487f479db5379e677031727.

--- src/components/select_user.js
+++
@@ -1,8 +0,0 @@
- const selectUser = (users) => {
-    console.log("You clicked on user: ", users);
-    return {
-        type: 'USER_SELECTED',
-        payload: users
-    }
-};
-export default selectUser;
